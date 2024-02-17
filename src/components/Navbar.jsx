4717a7384f9ce6616f92c25ae8d398092ab878c8
anurag <anuragbadwahe@gmail.com>
2024-02-17 15:26:31 +0530

    Navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <>
+      <div className="container mx-auto w-full">
+        <div className="bg-violet-300 min-h-20 flex items-center justify-center">
+        <h1 className="font-protest-riot text-lg transition-transform hover:scale-110">Todo App</h1>
+        </div>
+      </div>
+    </>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'protest-riot': ['Protest Riot', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
